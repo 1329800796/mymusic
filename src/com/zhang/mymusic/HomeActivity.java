e154125c70b20afba5c6addf47bb7aef6a2324eb
1329800796 <1329800796@qq.com>
2015-01-17 10:43:49 +0800

    第二次提交：“解析xml,显示在主界面”

--- src/com/zhang/mymusic/HomeActivity.java
+++ src/com/zhang/mymusic/HomeActivity.java
@@ -2,6 +2,7 @@ package com.zhang.mymusic;
 
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,26 +15,30 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
-
-import android.R.integer;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
 import com.zhang.mymusic.domain.Mp3Info;
 import com.zhang.mymusic.downloder.HttpDownloder;
 import com.zhang.mymusic.xml.Mp3ListContentHandler;
 
 public class HomeActivity extends Activity {
 
-	private static final int UPDATE = 1;//菜单控制常量
+	private static final int UPDATE = 1;// 菜单控制常量
 	private static final int ABOUT = 2;
+	private ListView listview;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_activity);
+		listview = (ListView) findViewById(R.id.list);
+		updateListView();
 	}
 
 	/**
@@ -42,8 +47,8 @@ public class HomeActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		menu.add(0,UPDATE, 1, R.string.mp3list_update);
-		menu.add(0,ABOUT, 2, R.string.mp3list_about);
+		menu.add(0, UPDATE, 1, R.string.mp3list_update);
+		menu.add(0, ABOUT, 2, R.string.mp3list_about);
 		return true;
 	}
 
@@ -53,43 +58,72 @@ public class HomeActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-		if(item.getItemId()== UPDATE){
-			//用户点击更新
-			Toast.makeText(HomeActivity.this, "gengxin", 1).show();
-			String xml =downloaderXML("http://192.168.1.109:8080/mp3/resources.xml");
-			parse( xml);
-			System.out.println("->>"+xml);
-		}else if(item.getItemId()== ABOUT){
-			//用户点击关于
+		if (item.getItemId() == UPDATE) {
+			// 用户点击更新
+			updateListView();
+		} else if (item.getItemId() == ABOUT) {
+			// 用户点击关于
 			Toast.makeText(HomeActivity.this, "guanyu", 1).show();
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	private String downloaderXML(String url){
+
+	/**
+	 * 更新
+	 */
+	private void updateListView() {
+		// 下载基本信息xml
+		String xml = downloaderXML("http://192.168.1.100:8080/mp3/resources.xml");
+		// 解析xml，并且封装成MP3list
+		List<Mp3Info> mp3infolist = parse(xml);
+		List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+
+		for (Iterator iterator = mp3infolist.iterator(); iterator.hasNext();) {
+			Mp3Info mp3info = (Mp3Info) iterator.next();
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("mp3_name", mp3info.getMp3Name());
+			map.put("mp3_size", mp3info.getMp3Size());
+			list.add(map);
+		}
+
+		SimpleAdapter simpleAdapter = new SimpleAdapter(this, list,
+				R.layout.mp3info_item, new String[] { "mp3_name", "mp3_size" },
+				new int[] { R.id.mp3_name, R.id.mp3_size });
+		listview.setAdapter(simpleAdapter);
+	}
+
+	/**
+	 * 下载xml
+	 * 
+	 * @param url
+	 * @return
+	 */
+	private String downloaderXML(String url) {
 		HttpDownloder downloder = new HttpDownloder();
-		String result = downloder.download(url);	
+		String result = downloder.download(url);
 		return result;
 	}
-	
-	private List<Mp3Info> parse(String xml){
+
+	/**
+	 * 解析封装
+	 * 
+	 * @param xml
+	 * @return
+	 */
+	private List<Mp3Info> parse(String xml) {
 		List<Mp3Info> infolist = new ArrayList<Mp3Info>();
 		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 		try {
 			XMLReader reader = saxParserFactory.newSAXParser().getXMLReader();
-			
-			Mp3ListContentHandler contentHandler = new Mp3ListContentHandler(infolist);
+
+			Mp3ListContentHandler contentHandler = new Mp3ListContentHandler(
+					infolist);
 			reader.setContentHandler(contentHandler);
 			reader.parse(new InputSource(new StringReader(xml)));
-			for (Iterator iterator = infolist.iterator(); iterator.hasNext();) {
-				Mp3Info mp3Info = (Mp3Info) iterator.next();
-				System.out.println(mp3Info);
-			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		return infolist;
-		
+
 	}
 }
-
